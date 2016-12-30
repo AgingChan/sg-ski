7119f651392403657e9354001d4fb918cc14b93c
Aging Chan <aging.chan@gmail.com>
2016-12-30 20:58:53 +0800

    Get the map

--- sg-ski.js
+++ sg-ski.js
@@ -9,6 +9,15 @@ var fs = require('fs'),
 	semver = require('semver'),
 	readline = require('readline');
 // Check the Node version
+
+const RETURN_ERR = {
+	SUCCESS: 0,
+	ERR_INVALID_ARGUMENTS: 1,
+	ERR_ILLEGAL_MAP: 2,
+}
+
+
+
 if(semver.lt(process.version,'6.0.0')){
 	console.log('Node version mismatch');
 	process.exit(1);
@@ -20,7 +29,7 @@ var filePath = '';
 if(process.argv.length > 3){
 	console.log("Unexpected arguments");
 	console.log("Please try this 'node sg-ski.js [map file]'");
-	process.exit(1);
+	process.exit(RETURN_ERR.ERR_INVALID_ARGUMENTS);
 }
 else if(process.argv.length === 3){
 	filePath = process.argv[2];
@@ -30,46 +39,77 @@ else{
 }
 
 console.log('Map File: ' + filePath);
-let rowNum = 0;
-let colNum = 0;
-let resortMap = new Array(0);
-
-var rd = readline.createInterface({
-	input: fs.createReadStream(filePath),
-})
-
-rd.on('line', (line) => {
-	arguments.callee.line = ++arguments.callee.line || 0;
-	if(arguments.callee.line == 0){
-		// The first line which stores the dimension
-		var dimension = line.split(' ');
-		rowNum = dimension[0];
-		colNum = dimension[1];
-		console.log('Row: ' + rowNum + '; Col: ' + colNum);
-	}
-	else{
-		resortMap.push(line.split(' '));
-		//console.log(line.split(' '));
-		if(arguments.callee.line >= colNum){
-			// The last line
-			console.log(resortMap);
-		}
-	}
-})
-
-
 
 function SG_SKI_RESOUT(mapFilePath){
 	let that = this;
 	this.mapArray = new Array(0);
-	this.mapFilePath = mapFile;
+	this.mapFilePath = mapFilePath;
+	this.rowNum = 0;
+	this.colNum = 0;
+
+	this.bestPath = {
+		"srcArea": {
+			"row":0,
+			"col":0,
+		},
+		"dstArea": {
+			"row": 0,
+			"col": 0,
+		},
+		"pathLength": 0,
+	};
+
 	this.mapReadlineInterface = readline.createInterface({
-		input: fs.createReadStream(that.mapFilePath);
+		input: fs.createReadStream(that.mapFilePath)
 	})
 
-	this.mapReadlineInterface.on('line' (line) =>{
-		
+	this.mapReadlineInterface.on('line', (line) => {
+		arguments.callee.line = ++arguments.callee.line || 0;
+		if(arguments.callee.line == 0){
+			// The first line which stores the dimension
+			var dimension = line.split(' ');
+			this.rowNum = dimension[0];
+			this.colNum = dimension[1];
+			console.log('Row: ' + this.rowNum + '; Col: ' + this.colNum);
+		}
+		else{
+			var numBufferString = line.split(' ');
+			if(numBufferString.length != this.colNum){
+				console.log('Illegal Ski Resort Map. Please double check the map file');
+				process.exit(RETURN_ERR.ERR_ILLEGAL_MAP);
+			}
+
+			this.mapArray.push(numBufferString);
+		}
+	})
+
+	this.mapReadlineInterface.on('close', () => {
+		if(this.mapArray.length != this.rowNum){
+			console.log('Illegal Ski Resort Map. Please double check the map file');
+			process.exit(RETURN_ERR.ERR_ILLEGAL_MAP);
+		}
+		else{
+			console.log('End of file and row num is correct');
+			this.getMapBestPath();
+		}
 	})
 }
 
+SG_SKI_RESOUT.prototype.isPeakArea = function(row, col) {
+	// body...
+};
 
+
+SG_SKI_RESOUT.prototype.isBottomArea = function(row, col) {
+	// body...
+};
+
+
+SG_SKI_RESOUT.prototype.getMapBestPath = function() {
+	// body...
+	console.log('To get the best path of the resort');
+	console.log(this.mapArray);
+};
+
+
+var resort = new SG_SKI_RESOUT(filePath);
